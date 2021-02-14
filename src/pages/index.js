1dc1ef64dd98e9fbc9fd783cfd60ce49f73cfd9a
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2021-02-14 11:22:41 -0700

    Update home page's color array

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,13 @@ export default function IndexPage() {
   const colorPairs = [
     [`0C0C0C`, `0F971C`],
     [`6D90B9`, `BBC7DC`],
+    [`6D90B9`, `BBC7DC`],
+    [`A04970`, `3B7F99`],
+    [`C9DEF4`, `B8A4C9`],
     [`D8BDBE`, `CC575F`],
     [`EBF4F5`, `B5C6E0`],
-    [`6D90B9`, `BBC7DC`],
-    [`C9DEF4`, `B8A4C9`],
-    [`A04970`, `3B7F99`],
+    // Pantone color of the year 2021
+    [`F5DF4D`, `939597`],
   ]
   const [colorPair, setColorPair] = useState(colorPairs[0])
   useEffect(() => {
